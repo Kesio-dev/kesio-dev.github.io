5c2ab2c42b716056dca9c66fc7ff0df32529a119
Kesio-dev <kesio.dev@gmail.com>
2022-07-02 18:40:38 +0500

    main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import './App.css';
-
+import styles from './main.module.scss'
 function App() {
   return (
-    <div className="App">
-      My portfolio
+    <div className={styles.main}>
+      <div className={styles.headerWrapper}>
+        <div className={styles.header}>
+          <div className={styles.logo}>SYP</div>
+          <div className={styles.rightHeader}>
+            <div className="search">
+              <div className="searchIcon searchIcon_search">
+                <input className="searchInput" type="text" placeholder="Search" />
+              </div>
+            </div>
+            <a href="#" className="btn">Вход</a>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.hero}>
+        <div className={styles.text}>
+          <div className={styles.title}>Создай свое портфолио прямо сейчас!</div>
+          <div className={styles.subtitle}>Теперь вы можете с легкостью создать и поделиться своим портфолио прямо сейчас</div>
+          <a href="#" className="btn active big">Подробнее</a>
+        </div>
+        <div className={styles.img}>
+          <img src="https://illustrators.ru/uploads/home_slider/image/9/main_Без_имени-1.jpg" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import './reset.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+
+import './ui.scss'
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
